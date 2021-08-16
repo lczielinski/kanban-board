1a2d945be2aed312a862b9d2ed777dc67ebb59c3
Laura Zielinski <lzielinski@uchicago.edu>
2021-08-16 14:03:05 -0400

    finish modal design

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,20 +5,36 @@ const Board = () => (
   <div>
     {/* Modal to add a new card */}
     <div className="modal fade" id="modal" tabIndex="-1" role="dialog">
-      <div className="modal-dialog" role="document">
+      <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">Modal title</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <h5 className="modal-title">Add a new card</h5>
+            <div type="button" className="close" data-dismiss="modal">
               <span>&times;</span>
-            </button>
+            </div>
           </div>
+
           <div className="modal-body">
-            ...
+            <form className="form-group">
+              <div className="form-group row">
+                <label htmlFor="cardTitle" className="col m-1">
+                  Task title
+                  <input type="text" className="form-control my-1" id="cardTitle" placeholder="Title" />
+                </label>
+              </div>
+
+              <div className="form-group row">
+                <label htmlFor="cardBody" className="col m-1">
+                  Task description
+                  <textarea row="3" type="text" className="form-control my-1" id="cardBody" placeholder="Description..." />
+                </label>
+              </div>
+            </form>
           </div>
+
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary">Save changes</button>
+            <div type="button" className="btn btn-secondary" data-dismiss="modal">Close</div>
+            <div type="button" className="btn btn-primary">Add Card</div>
           </div>
         </div>
       </div>
