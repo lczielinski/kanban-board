9317a0bd6053ff60e88f88118130fbcfda922697
Laura Zielinski <lzielinski@uchicago.edu>
2021-08-17 22:01:46 -0400

    remove frontend node_modules

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Login = () => {
+  
+  return (
+    <div className="container">
+
+    </div>
+  );
+};
+
+export default Login;
